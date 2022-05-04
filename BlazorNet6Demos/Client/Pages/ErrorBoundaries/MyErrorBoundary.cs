74c80b0c50918c1488038161e1d7a0d1e34e29c3
Robert Haken <haken@havit.cz>
2022-05-04 11:03:56 +0200

    ErrorBoundaries - split demos to individual pages + new MyErrorBoundary component demo

---
+++ BlazorNet6Demos/Client/Pages/ErrorBoundaries/MyErrorBoundary.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorNet6Demos.Client.Pages.ErrorBoundaries
+{
+	public class MyErrorBoundary : ErrorBoundary // ErrorBoundaryBase
+	{
+		protected override Task OnErrorAsync(Exception exception)
+		{
+			Console.WriteLine("ERROR: " + exception.Message);
+
+			// base.OnErrorAsync(exception); ==> await ErrorBoundaryLogger.LogErrorAsync(exception);
+
+			return Task.CompletedTask;
+		}
+	}
+}
